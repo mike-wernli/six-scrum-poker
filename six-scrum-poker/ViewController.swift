2ce8dd8a5ebcbe5ae3a02c2a92b42acc9d382179
Mike Wernli <mike.wernli@gmail.com>
2016-06-08 21:04:56 +0200

    Initial Commit

---
+++ six-scrum-poker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  six-scrum-poker
+//
+//  Created by codecamp on 08.06.16.
+//  Copyright © 2016 six.codecamp16. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
